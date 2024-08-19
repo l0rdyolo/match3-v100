389822d92a4e45a67edbb0cf9d537a1195755c11
l0rdyolo <wp.omer820@gmail.com>
2024-08-20 00:02:08 +0300

    initial commit

---
+++ temp/programming/packer-driver/targets/preview/chunks/03/034bdd4d252e771cc797692c7adec90ad830b5d8.js
@@ -0,0 +1,27 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, _crd;
+
+  function sleep(ms, result) {
+    return new Promise(resolve => setTimeout(() => resolve(result), ms));
+  }
+
+  _export("sleep", sleep);
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "deaf1cf89BH2ZlcgCLgKE6f", "Promises", undefined);
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=034bdd4d252e771cc797692c7adec90ad830b5d8.js.map
